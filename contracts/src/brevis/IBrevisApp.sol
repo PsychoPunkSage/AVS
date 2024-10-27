adcae53bde3dd93d9d3478a462c222c03f15ab47
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-10-27 16:52:02 +0530

    fix::> Added Brevis Interfaces

---
+++ contracts/src/brevis/IBrevisApp.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "./lib/Lib.sol";
+
+interface IBrevisApp {
+    function callback(
+        bytes32 _requestId,
+        bytes calldata _appCircuitOutput
+    ) external;
+}
